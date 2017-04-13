a5d89bab0a76ef9bdabd3cfeabb429775fa60dbb
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-04-13 13:13:40 +0200

    prepare some basics Alarm managers

--- app/src/main/java/com/softwareacademy/maps/fcmproject/fcm/MyFcmMessengingService.java
+++ app/src/main/java/com/softwareacademy/maps/fcmproject/fcm/MyFcmMessengingService.java
@@ -1,16 +1,21 @@
 package com.softwareacademy.maps.fcmproject.fcm;
 
+import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.softwareacademy.maps.fcmproject.MainActivity;
+import com.softwareacademy.maps.fcmproject.receiver.AlarmReceiver;
+
+import java.util.Calendar;
 
 /**
  *
@@ -18,10 +23,31 @@ import com.softwareacademy.maps.fcmproject.MainActivity;
 
 public class MyFcmMessengingService extends FirebaseMessagingService {
 
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         remoteMessage.getNotification().getBody();
+
+        AlarmManager alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        /* Repeating Alarm*/
+        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() /*kiedy*/ ,
+                1000 /*co ile powtarzac*/, alarmIntent /*Pending Intent*/);
+
+
+        // Set the alarm to start at 8:30 a.m.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 8);
+        calendar.set(Calendar.MINUTE, 30);
+
+        alarmMgr.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmIntent);
     }
 
 
---
+++ app/src/main/java/com/softwareacademy/maps/fcmproject/receiver/AlarmReceiver.java
@@ -0,0 +1,17 @@
+package com.softwareacademy.maps.fcmproject.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ *
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("RECEIVER_FROM_ALARM","IT IS ALIVE !!!!!");
+    }
+}
