7b499590ba342ba602f10cc5fe5ed0c059003cfa
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-04-13 12:45:12 +0200

    add services for FCM

---
+++ app/src/main/java/com/softwareacademy/maps/fcmproject/fcm/MyFcmMessengingService.java
@@ -0,0 +1,16 @@
+package com.softwareacademy.maps.fcmproject.fcm;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ *
+ */
+
+public class MyFcmMessengingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
