8e722cefc90098c0ded069fe6591710e7fe83f48
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-04-13 13:00:10 +0200

    add showing notyfication

--- app/src/main/java/com/softwareacademy/maps/fcmproject/fcm/MyFcmMessengingService.java
+++ app/src/main/java/com/softwareacademy/maps/fcmproject/fcm/MyFcmMessengingService.java
@@ -1,7 +1,16 @@
 package com.softwareacademy.maps.fcmproject.fcm;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.softwareacademy.maps.fcmproject.MainActivity;
 
 /**
  *
@@ -12,5 +21,30 @@ public class MyFcmMessengingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        remoteMessage.getNotification().getBody();
     }
+
+
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.arrow_up_float)
+                .setContentTitle("FCM Message")
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
 }
--- app/src/main/java/com/softwareacademy/maps/fcmproject/fcm/TokenService.java
+++ app/src/main/java/com/softwareacademy/maps/fcmproject/fcm/TokenService.java
@@ -18,7 +18,7 @@ public class TokenService extends FirebaseInstanceIdService {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
-
+        //e1KIINH2Av8:APA91bHvL19ew9J3XKKrDMlRG-ssbN1pnlfoDUGqVOp5fZEgFPPWN2ETvOHcJ3R2o_9e_TR9l8PP-ZND648qnPtuawJN2fhilLWx-XvVtHOkjLbKL9yXynwj5tSgEM3kN_ztwxRQh7W3
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
